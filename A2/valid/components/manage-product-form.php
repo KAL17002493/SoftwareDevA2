05ba05238f9d1881be013f33eaf3d069ad8d99f1
KAL17002493 <74010320+KAL17002493@users.noreply.github.com>
2023-01-10 14:01:51 +0000

    trying to make edit product work

--- A2/valid/components/manage-product-form.php
+++ A2/valid/components/manage-product-form.php
@@ -1,25 +1,102 @@
-<?php
-require_once './inc/functions.php';
-
- $products =$controllers->products()->getAll();
-
-foreach ($products as $product):
-?>
-    <div class="col-4 my-2 text-center">
-        <div class="card">
-            <img src="<?= $product['image'] ?>" 
-                class="card-img-top" 
-                alt="$product['name']">
-            <div class="card-body">
-                <h5 class="card-title"><?= $product['name'] ?></h5>
-                <p class="card-text"><?= $product['description'] ?></p>
-                <p class="card-text">£<?= $product['price'] ?></p>
-                <button type="button" class="btn btn-info w-100">Edit</button>
-            </div>
-        </div>
-    </div>
 <?php 
-endforeach;
+
+require_once './inc/functions.php';
+$message = '';
+
+if (isset($_GET["id"]))
+{
+    $productId = htmlspecialchars($_GET["id"]);
+    $product = $controllers->products()->get($productId);
+
+    $name = InputProcessor::process_string($_POST['name'] ?? '');
+    $description = InputProcessor::process_string($_POST['description'] ?? '');
+    $price = InputProcessor::process_string($_POST['price'] ?? '');
+    $image = InputProcessor::process_file($_FILES['image'] ?? []);
+
+    $valid =  $name['valid'] && $description['valid'] && $price['valid'] && $image['valid'];
+
+    var_dump($product);
+
+    if($valid) {
+
+      $image['value'] = ImageProcessor::upload($_FILES['image']);
+      
+      $valueCheck = [
+        'name' => 'name',
+        'description' => 'description',
+        'price' => 'price',
+        'image' => 'image'];
+    
+    $args = [];
+    foreach ($valueCheck as $input => $value) 
+    {
+        if (${$input}['value'] !== $product[$value])
+        {
+            $args[$value] = ${$input}['value'];
+        }
+    }
+
+    if (!empty($args)) {
+        $id = $controllers->products()->update($args);
+    }
+
+      if(!empty($id) && $id > 0) {
+        redirect('edit-product', ['id' => $id]);
+      }
+      else {
+        $message = "Error adding product."; //Change
+      }
+    }
+    else {
+       $message =  "Please fix the following errors: ";
+   }
+
+}
+
 ?>
 
+  <form method="post" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" enctype="multipart/form-data">
+    <section class="vh-100">
+      <div class="container py-5 h-75">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card shadow-2-strong" style="border-radius: 1rem;">
+              <div class="card-body p-5 text-center">
+    
+                <h3 class="mb-2">Edit Product</h3>
+                <div class="form-outline mb-4">
+                  <input type="text" id="name" name="name" class="form-control form-control-lg" required value="<?= htmlspecialchars($product['name'] ?? "") ?>"/>
+                  <span class="text-danger"><?= $name['error'] ?? '' ?></span>
+                </div>
+                
+                <div class="form-outline mb-4">
+                  <input type="text" id="description" name="description" class="form-control form-control-lg" required value="<?= htmlspecialchars($product['description'] ?? " ") ?>"/>
+                  <span class="text-danger"><?= $description['error'] ?? '' ?></span>
+                </div>
+    
+                <div class="form-outline mb-4">
+                  <input type="number" id="price" name="price" class="form-control form-control-lg" required value="<?= htmlspecialchars($product['price'] ?? " ") ?>"/>
+                  <span class="text-danger"><?= $price['error'] ?? '' ?></span>
+                </div>
 
+                <div class="form-outline mb-4">
+                <img src="<?= $product['image'] ?>" class="card-img-top" alt="$product['name']">
+                <span class="text-danger"><?= $price['error'] ?? '' ?></span>
+                </div>
+    
+                <div class="form-outline mb-4">
+                  <input type="file" accept="image/*" id="image" name="image" class="form-control form-control-lg" placeholder="Select Image"required />
+                </div>
+    
+                <button class="btn btn-primary btn-lg w-100 mb-4" type="submit">Save</button>
+               
+                <?= isset($_GET['errmsg']) ? $message = $_GET['errmsg'] : '' ?>
+                <?= $message ? alert($message, 'danger') : '' ?>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </form>
--- A2/valid/edit-product.php
+++ A2/valid/edit-product.php
@@ -12,7 +12,7 @@ if ($_SESSION['user']['role'] != "admin")
 <section class="vh-100 text-center">
     <div class="container py-5 h-75">
       <div class="row d-flex align-items-center h-100">
-         <?php require __DIR__ . "/components/manage-product-overview.php"; ?>
+         <?php require __DIR__ . "/components/manage-product-form.php"; ?>
       </div>
     </div>
 </section>  
